af8b772e84306ddfce9510f8447a302c1463f717
MariaPantea <mariapantea87@gmail.com>
2020-12-15 20:23:00 +0100

    Switch dubble undescores for '.' in automations field names

--- models/base/mailchimp_automations.sql
+++ models/base/mailchimp_automations.sql
@@ -16,42 +16,42 @@ with base as (
         -- email details
         status,
         emails_sent,
-        settings__from_name as from_name,
-        settings__reply_to as reply_to,
-        settings__title as title,
-        settings__to_name as to_name,
-        settings__authenticate as authenticate,
-        settings__auto_footer as auto_footer,
-        settings__inline_css as inline_css,
-        settings__use_conversation as use_conversation,
+        settings.from_name as from_name,
+        settings.reply_to as reply_to,
+        settings.title as title,
+        settings.to_name as to_name,
+        settings.authenticate as authenticate,
+        settings.auto_footer as auto_footer,
+        settings.inline_css as inline_css,
+        settings.use_conversation as use_conversation,
 
         -- tracking
-        tracking__clicktale as clicktale,
-        tracking__google_analytics as google_analytics,
-        tracking__ecomm360 as track_ecomm_360,
-        tracking__goal_tracking as track_goals,
-        tracking__html_clicks as track_html_clicks,
-        tracking__opens as track_opens,
-        tracking__text_clicks as track_text_clicks,
+        tracking.clicktale as clicktale,
+        tracking.google_analytics as google_analytics,
+        tracking.ecomm360 as track_ecomm_360,
+        tracking.goal_tracking as track_goals,
+        tracking.html_clicks as track_html_clicks,
+        tracking.opens as track_opens,
+        tracking.text_clicks as track_text_clicks,
 
         -- recipients
-        recipients__list_id as recipients_list_id,
-        recipients__list_is_active as recipients_list_is_active,
-        recipients__list_name as recipients__list_name,
+        recipients.list_id as recipients_list_id,
+        recipients.list_is_active as recipients_list_is_active,
+        recipients.list_name as recipients__list_name,
 
         -- trigger
-        trigger_settings__workflow_type as workflow_type,
-        trigger_settings__runtime as runtime,
-        trigger_settings__workflow_title as workflow_title,
-        trigger_settings__workflow_emails_count as workflow_emails_count,
+        trigger_settings.workflow_type as workflow_type,
+        trigger_settings.runtime as runtime,
+        trigger_settings.workflow_title as workflow_title,
+        trigger_settings.workflow_emails_count as workflow_emails_count,
 
         -- report
-        report_summary__opens as opens,
-        report_summary__unique_opens as unique_opens,
-        report_summary__open_rate as open_rate,
-        report_summary__clicks as clicks,
-        report_summary__subscriber_clicks as subscriber_clicks,
-        report_summary__click_rate as click_rate
+        report_summary.opens as opens,
+        report_summary.unique_opens as unique_opens,
+        report_summary.open_rate as open_rate,
+        report_summary.clicks as clicks,
+        report_summary.subscriber_clicks as subscriber_clicks,
+        report_summary.click_rate as click_rate
 
     from base
 
