046dd22567133ff9b6835886d389bc2ce0ddc36b
MariaPantea <mariapantea87@gmail.com>
2020-12-15 20:33:15 +0100

    Switch dubble underscore to '.' in all field names

--- models/base/mailchimp_list_members.sql
+++ models/base/mailchimp_list_members.sql
@@ -17,8 +17,8 @@ with base as (
         status,
         unsubscribe_reason,
 
-        stats__avg_open_rate as avg_open_rate,
-        stats__avg_click_rate as avg_click_rate,
+        stats.avg_open_rate as avg_open_rate,
+        stats.avg_click_rate as avg_click_rate,
 
         ip_signup,
         timestamp_signup,
@@ -30,12 +30,12 @@ with base as (
         vip,
         email_client,
 
-        location__latitude as latitude,
-        location__longitude as longitude,
-        location__gmtoff as gmtoff,
-        location__dstoff as dstoff,
-        location__country_code as country_code,
-        location__timezone as timezone
+        location.latitude as latitude,
+        location.longitude as longitude,
+        location.gmtoff as gmtoff,
+        location.dstoff as dstoff,
+        location.country_code as country_code,
+        location.timezone as timezone
 
     from base
 
